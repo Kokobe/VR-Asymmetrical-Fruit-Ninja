350343f0d06063a64e84e2df938c913e0ec471d5
Kokobe <KobeChang@Kobes-MacBook-Pro.local>
2019-02-08 12:57:32 -0800

    File-Reordering

---
+++ Assymetrical_Fruit_Ninja/Assets/UCI_FruitNinja/Scripts/ChangeSkyBoxOvertime.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSkyBoxOvertime : MonoBehaviour
+{
+    public Material sky;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // DynamicGI.UpdateEnvironment();
+
+        StartCoroutine(updateLightning());
+    }
+
+    public Color sundownTop = new Color(236f / 255, 223f / 255, 148f / 255);
+    public Color sundownBottom = new Color(224f / 255, 106f / 255, 92f / 255);
+
+    public Color nightTop = new Color();
+    public Color nightBottom = new Color();
+    // Update is called once per frame
+    float t = 0f;
+
+    IEnumerator updateLightning()
+    {
+        while (t < 1f)
+        {
+            DynamicGI.UpdateEnvironment();
+            yield return new WaitForSeconds(.25f);
+
+
+        }
+
+    }
+    public bool Sundown = true;
+    public bool Night = false;
+    bool changingSundownToNight = true;
+    bool changingNightToSundown = false;
+
+    IEnumerator flipBooleans()
+    {
+        print("delayd");
+        Night = !Night;
+        Sundown = !Sundown;
+        foreach (GlowWhenNight g in FindObjectsOfType<GlowWhenNight>())
+        {
+            if (Night)
+                StartCoroutine(g.startGlowing());
+        }
+        yield return new WaitForSeconds(20f);
+        changingSundownToNight = !changingSundownToNight;
+        changingNightToSundown = !changingNightToSundown;
+
+        foreach (GlowWhenNight g in FindObjectsOfType<GlowWhenNight>())
+        {
+            if (changingNightToSundown)
+                StartCoroutine(g.stopGlowing());
+        }
+
+        firstTime = true;
+        t = 0;
+        StartCoroutine(updateLightning());
+    }
+
+    bool firstTime = true;
+
+    void Update()
+    {
+   
+
+        if (t >= 1 && firstTime)
+        {
+            //add in a delay ienumerator
+            StartCoroutine(flipBooleans());
+
+            firstTime = false;
+        }
+        else
+        {
+            if (Sundown && changingSundownToNight)
+            {
+                sky.SetColor("_Color2", Color.Lerp(sundownTop, nightTop, t)); //top
+                sky.SetColor("_Color1", Color.Lerp(sundownBottom, nightBottom, t));
+                //sky.SetFloat("Intensity", 100f);
+
+                t += Time.deltaTime * .05f;
+
+            }
+            else if (Night && changingNightToSundown)
+            {
+                sky.SetColor("_Color2", Color.Lerp(nightTop, sundownTop, t)); //top
+                sky.SetColor("_Color1", Color.Lerp(nightBottom, sundownBottom, t));
+                //sky.SetFloat("Intensity", 100f);
+
+                t += Time.deltaTime * .05f;
+            }
+        }
+
+
+
+    }
+}
---
+++ Assymetrical_Fruit_Ninja/Assets/UCI_FruitNinja/Scripts/GlowWhenNight.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowWhenNight : MonoBehaviour
+{
+    public ChangeSkyBoxOvertime sky; 
+    public Material glowingPart;
+    public Color glowColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        glowColor = glowColor * Mathf.LinearToGammaSpace(24f);
+        glowingPart.SetColor("_EmissionColor", Color.black);
+    }
+
+
+    public IEnumerator startGlowing()
+    {
+        float t = 0f;
+        while(t <= 1f)
+        {
+            glowingPart.SetColor("_EmissionColor", Color.Lerp(Color.black, glowColor, t));
+            t += Time.deltaTime;
+            yield return null;
+        }
+
+    }
+
+    public IEnumerator stopGlowing()
+    {
+        float t = 0f;
+        while (t <= 1f)
+        {
+           
+            glowingPart.SetColor("_EmissionColor", Color.Lerp(glowColor, new Color(0, 0, 0, 0), t));
+            t += Time.deltaTime;
+            yield return null;
+        }
+        ///yield return null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //glowingPart.SetColor("_EmissionColor", Color.Lerp(Color.black, glowColor,.1f));
+    }
+}
