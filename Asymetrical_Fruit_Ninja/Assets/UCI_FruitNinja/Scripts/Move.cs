1ad4b89b5268a5620c32ee245a767cb3d0c07d86
Kokobe <KobeChang@dhcp-10-8-039-147.mobile.reshsg.uci.edu>
2019-04-24 19:42:12 -0700

    redone

---
+++ Asymetrical_Fruit_Ninja/Assets/UCI_FruitNinja/Scripts/Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public int speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("hello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody>().velocity += new Vector3(0, speed, 0);
+        }
+    }
+}
---
+++ Asymetrical_Fruit_Ninja/Assets/UCI_FruitNinja/Scripts/ShootFruit.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootFruit : MonoBehaviour
+{
+    public Transform central_aim;
+    public Transform right_aim;
+    public Transform left_aim;
+    public float velocity = 10f;
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(Input.mouseScrollDelta);
+        if (Input.GetMouseButtonDown(0))
+        {
+            var go = Instantiate(prefab, left_aim.position, Quaternion.identity);
+            go.GetComponent<Rigidbody>().velocity = (central_aim.position - transform.position).normalized * velocity + GetComponent<Rigidbody>().velocity;
+            Destroy(go, 8);
+
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            var go = Instantiate(prefab, right_aim.position, Quaternion.identity);
+            go.GetComponent<Rigidbody>().velocity = (central_aim.position - transform.position).normalized * velocity + GetComponent<Rigidbody>().velocity;
+            Destroy(go, 8);
+
+        }
+    }
+}
